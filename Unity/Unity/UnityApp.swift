d3fa1808e9a669ddf9a8d674c77801832338d862
Samuel Kosasih <samadrianko@gmail.com>
2023-03-29 13:23:43 -0400

    Initial Commit

---
+++ Unity/Unity/UnityApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnityApp.swift
+//  Unity
+//
+//  Created by Samuel Kosasih on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct UnityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
