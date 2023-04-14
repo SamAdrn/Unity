236ec89ca6b09b969eae3af6d2d936f0f523376f
Samuel Kosasih <samadrianko@gmail.com>
2023-04-14 17:21:31 -0400

    Added a little transparent rectangle to show stats on a map.

--- Unity/Unity/MapView.swift
+++ Unity/Unity/MapView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 38.9897, longitude: -76.9378), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 38.9897, longitude: -76.9378), span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
 
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
-            .frame(width: 400, height: 500)
+        
+        VStack {
+            Text("Unity")
+                .font(.system(size: 48, weight: .bold))
+                .padding(20)
+            
+            Spacer()
+            
+            
+            ZStack (alignment: .bottomLeading) {
+                
+                Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+                    .frame(width: 400, height: 500)
+                
+                Rectangle()
+                    .opacity(0.3)
+                    .frame(width: 100, height: 150)
+                    .padding(30)
+            }
+            
+            Spacer()
+        }
+        
     }
 }
 
