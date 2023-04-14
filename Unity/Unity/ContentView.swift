775245d55fa58a407a145525940ada1ffaa92685
Isaac Wu <isaacwu@wireless-10-104-83-136.umd.edu>
2023-04-14 16:27:24 -0400

    Tab Views Added

--- Unity/Unity/ContentView.swift
+++ Unity/Unity/ContentView.swift
@@ -13,8 +13,25 @@ struct ContentView: View {
         ZStack {
 //            MapView()
             ProfileView()
+            TbView()
         }
         
+    }
+}
+struct TbView: View {
+    //@EnvironmentObject var game: Triples
+    var body: some View {
+        TabView {
+            ProfileView().tabItem {
+                Label("pf", systemImage: "gamecontroller")
+            }
+            MapView().tabItem {
+                Label("map", systemImage: "list.dash")
+            }
+            /*HeaderView.tabItem {
+                Label("a", systemImage: "info.circle")
+            }*/
+        }
     }
 }
 
