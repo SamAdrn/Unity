86ede14bdece0ecf9e1a73af1c7df935327ab8f5
Matthew Ray Lee <matthewraylee@gmail.com>
2023-04-14 19:13:19 -0400

    Added Step tracker feature

--- Unity/Unity/ContentView.swift
+++ Unity/Unity/ContentView.swift
@@ -8,34 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var vm: Unity
+    
     var body: some View {
         ZStack {
-            TbView()
+            TbView().environmentObject(vm)
         }
         
     }
 }
 struct TbView: View {
-    
+    @EnvironmentObject var vm: Unity
     @State private var selection = 1
     private var iconSize: CGFloat = 1000
     
     var body: some View {
         TabView(selection: $selection) {
-            
+
             Group { Text("Camera") }.tabItem {
                 Image(systemName: "camera")
                     .font(.title)
             }.tag(1)
-            
-            MapView().tabItem {
+
+            MapView().environmentObject(vm).tabItem {
                 Image(systemName: "house")
             }.tag(2)
-            
+
             ProfileView().tabItem {
                 Image(systemName: "person.crop.circle")
             }.tag(3)
-            
+
         }.onAppear {
             selection = 2
         }
@@ -44,6 +46,6 @@ struct TbView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(Unity())
     }
 }
