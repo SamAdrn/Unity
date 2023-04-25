40e4edb4b0ead885347052d5bb773a9f7f9ac5a8
Samuel Kosasih <samadrianko@gmail.com>
2023-04-25 17:02:43 -0400

    Readded Files

---
+++ Unity/Unity/Model.swift
@@ -0,0 +1,57 @@
+//
+//  Model.swift
+//  Unity
+//
+//  Created by Matthew Ray Lee on 4/14/23.
+//
+
+import Foundation
+import HealthKit
+
+class Unity: ObservableObject {
+    // For Step Count
+    private var healthStore = HKHealthStore()
+    private var healthKitManager = HealthKitManager()
+    @Published var userStepCount = ""
+    @Published var isAuthorized = false
+    
+    init() {
+        changeAuthorizationStatus()
+    }
+    
+    // Authorization Prompt
+    func healthRequest() {
+        healthKitManager.setUpHealthRequest(healthStore: healthStore) {
+            self.changeAuthorizationStatus()
+            self.readStepsTakenToday()
+        }
+    }
+    
+    func changeAuthorizationStatus() {
+        guard let stepQtyType = HKObjectType.quantityType(forIdentifier: .stepCount) else { return }
+        let status = self.healthStore.authorizationStatus(for: stepQtyType)
+        
+        switch status {
+        case .notDetermined:
+            isAuthorized = false
+        case .sharingDenied:
+            isAuthorized = false
+        case .sharingAuthorized:
+            isAuthorized = true
+        @unknown default:
+            isAuthorized = false
+        }
+    }
+    
+    // Get step count
+    func readStepsTakenToday() {
+        healthKitManager.readStepCount(forToday: Date(), healthStore: healthStore) { step in
+            if step != 0.0 {
+                DispatchQueue.main.async {
+                    self.userStepCount = String(format: "%.0f", step)
+                }
+            }
+        }
+    }
+    
+}
